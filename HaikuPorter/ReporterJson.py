989a74ae32ef0f75a489a33fda8dff4b1539fd46
jurgenwigg <jurgenwigg@protonmail.com>
2023-07-20 10:14:16 +0000

    source code formatted with black

--- HaikuPorter/ReporterJson.py
+++ HaikuPorter/ReporterJson.py
@@ -12,17 +12,18 @@ from .Utils import sysExit, warn, info
 import json
 import os
 
+
 class ReporterJson(object):
-	def __init__(self, filename, branch, architecture):
-		self.filename = filename
-		self.branch = branch
-		self.architecture = architecture
+    def __init__(self, filename, branch, architecture):
+        self.filename = filename
+        self.branch = branch
+        self.architecture = architecture
 
-	def connected(self):
-		return True
+    def connected(self):
+        return True
 
-	def updateBuildrun(self, buildNumber, status):
-		tempFile = self.filename + '.temp'
-		with open(tempFile, 'w') as outputFile:
-			outputFile.write(json.dumps(status))
-			os.rename(tempFile, self.filename)
+    def updateBuildrun(self, buildNumber, status):
+        tempFile = self.filename + ".temp"
+        with open(tempFile, "w") as outputFile:
+            outputFile.write(json.dumps(status))
+            os.rename(tempFile, self.filename)
